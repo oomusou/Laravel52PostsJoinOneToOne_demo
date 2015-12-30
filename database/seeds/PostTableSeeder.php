d2030bee2512e01c1f586ecd86476e9f8b9600f0
oomusou <oomusou@gmail.com>
2015-12-30 15:52:19 +0800

    產生Post ModelFactory與User, Post TableSeeder

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Post;
+use Faker\Generator;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /** @var Generator */
+    private $faker;
+
+
+    /**
+     * PostTableSeeder constructor.
+     *
+     * @param Generator $faker
+     */
+    public function __construct(Generator $faker)
+    {
+        $this->faker = $faker;
+    }
+
+    public function run()
+    {
+        collect(range(1, 10))->each(function ($index) {
+            Post::create([
+                'user_id' => $index,
+                'title'   => $this->faker->sentence,
+                'content' => $this->faker->paragraph,
+            ]);
+        });
+
+    }
+}
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
-        $this->call(PostTableSeeder::class);
+        factory(User::class, 10)->create();
     }
 }
